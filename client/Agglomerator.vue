7e2a895df5f404c7219df140b3674f311f863f94
Levi Wheatcroft <gitcommits@lemail.au>
2024-05-16 05:47:44 +0800

    Agglomerator

--- client/Agglomerator.vue
+++ client/Agglomerator.vue
@@ -1,3 +1,18 @@
+<script>
 export default {
-  
+  props: {
+    items: {
+      type: Array,
+      default () { return [] }
+    }
+  }
 }
+</script>
+<template lang="pug">
+#agglomerator(
+  
+)
+</template>
+<style>
+
+</style>
